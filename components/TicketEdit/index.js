dac73e3432d79e1b173563138d2f21518b678db9
Nguyen Chinh Thuan <thuan.nguyen@thuocsi.vn>
2021-03-18 10:55:52 +0700

    Update Image size quality padding

--- components/TicketEdit/index.js
+++ components/TicketEdit/index.js
@@ -124,7 +124,7 @@ const TicketEdit = ({
             });
           }
         });
-        console.log(tmpData);
+
         setListAssignUser(tmpData);
       } else {
         setListAssignUser([{ value: '', label: '' }]);
@@ -461,22 +461,25 @@ const TicketEdit = ({
                       <div style={{ display: 'flex', flexDirection: 'row' }}>
                         {images.length !== 0 ? (
                           images.map((image) => (
-                            <div style={{ padding: '15px' }}>
-                              <div
-                                style={{
-                                  borderRadius: '5px',
-                                  border: '2px solid rgba(0, 0, 0, 0.87)',
-                                  lineHeight: '0.43',
-                                }}
-                              >
-                                <Image
-                                  src={image}
-                                  width="90"
-                                  height="90"
-                                  onClick={() => handleShowImage(image)}
-                                />
+                            <>
+                              <div style={{ paddingRight: '30px', paddingTop: '15px' }}>
+                                <div
+                                  style={{
+                                    borderRadius: '5px',
+                                    border: '2px solid rgba(0, 0, 0, 0.87)',
+                                    lineHeight: '0.43',
+                                  }}
+                                >
+                                  <Image
+                                    src={image}
+                                    quality={100}
+                                    width={220}
+                                    height={220}
+                                    onClick={() => handleShowImage(image)}
+                                  />
+                                </div>
                               </div>
-                            </div>
+                            </>
                           ))
                         ) : (
                           <div>Không có hình ảnh</div>
