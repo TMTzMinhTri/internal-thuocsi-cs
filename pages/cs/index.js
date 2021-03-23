3ed7e58684246bde90bf165ae70e41ca55109b8c
NamPH <nam.pham@thuocsi.vn>
2021-03-23 09:27:40 +0700

    Update

--- pages/cs/index.js
+++ pages/cs/index.js
@@ -12,68 +12,68 @@ import { LIMIT_DEFAULT, PAGE_DEFAULT } from 'data';
 import { TicketList } from 'components';
 
 export async function loadRequestData(ctx) {
-  // Fetch data from external API
+    // Fetch data from external API
 
-  const { query } = ctx;
-  const {
-    search = '',
-    page = PAGE_DEFAULT,
-    limit = LIMIT_DEFAULT,
-    action = '',
-    ...restProps
-  } = query;
-  const offset = page * limit;
+    const { query } = ctx;
+    const {
+        search = '',
+        page = PAGE_DEFAULT,
+        limit = LIMIT_DEFAULT,
+        action = '',
+        ...restProps
+    } = query;
+    const offset = page * limit;
 
-  const ticketClient = getTicketClient(ctx, {});
-  const filterValue = {
-    ...restProps,
-    ...(restProps?.reasons && { reasons: restProps?.reasons.split(',') }),
-  };
+    const ticketClient = getTicketClient(ctx, {});
+    const filterValue = {
+        ...restProps,
+        ...(restProps?.reasons && { reasons: restProps?.reasons.split(',') }),
+    };
 
-  // TODO offset limit
-  const [ticketResult, listReasonRes] = await Promise.all([
-    action === 'filter'
-      ? ticketClient.getTicketByFilterServer(filterValue)
-      : ticketClient.getList(offset, limit, search),
-    ticketClient.getListReason(),
-  ]);
-  const total = ticketResult?.total || 0;
-  const tickets = ticketResult?.data || [];
+    // TODO offset limit
+    const [ticketResult, listReasonRes] = await Promise.all([
+        action === 'filter'
+            ? ticketClient.getTicketByFilterServer(filterValue)
+            : ticketClient.getList(offset, limit, search),
+        ticketClient.getListReason(),
+    ]);
+    const total = ticketResult?.total || 0;
+    const tickets = ticketResult?.data || [];
 
-  const listReason = listReasonRes?.data?.map((item) => ({ value: item.code, label: item.name }));
-  return {
-    props: {
-      listReason,
-      total,
-      tickets,
-      action,
-      filter: { ...restProps },
-      formData: filterValue,
-    },
-  };
+    const listReason = listReasonRes?.data?.map((item) => ({ value: item.code, label: item.name }));
+    return {
+        props: {
+            listReason,
+            total,
+            tickets,
+            action,
+            filter: { ...restProps },
+            formData: filterValue,
+        },
+    };
 }
 
 export async function getServerSideProps(ctx) {
-  return doWithLoggedInUser(ctx, (cbCtx) => loadRequestData(cbCtx));
+    return doWithLoggedInUser(ctx, (cbCtx) => loadRequestData(cbCtx));
 }
 
 const breadcrumb = [
-  {
-    name: 'Trang chủ',
-    link: '/cs',
-  },
-  {
-    name: 'DS phiếu yêu cầu',
-  },
+    {
+        name: 'Trang chủ',
+        link: '/cs',
+    },
+    {
+        name: 'Danh sách phiếu hỗ trợ',
+    },
 ];
 
 const ListTicketPage = (props) => (
-  <AppCS select="/cs" breadcrumb={breadcrumb}>
-    <Head>
-      <title>DS phiếu yêu cầu</title>
-    </Head>
-    <TicketList {...props} />
-  </AppCS>
+    <AppCS select="/cs" breadcrumb={breadcrumb}>
+        <Head>
+            <title>Phiếu hỗ trợ</title>
+        </Head>
+        <TicketList {...props} />
+    </AppCS>
 );
 
 const index = (props) => renderWithLoggedInUser(props, ListTicketPage);
