dbe927242198ba8a62d75ae4541328c15d16c0cc
NamPH <nam.pham@thuocsi.vn>
2021-03-24 08:53:53 +0700

    Fix

--- components/index.js
+++ components/index.js
@@ -1,4 +1,1 @@
-export { default as TicketList } from './TicketList';
-export { default as TicketEdit } from './TicketEdit';
-export { default as TicketTable } from './TicketTable';
 export { default as LabelFormCs } from './LabelFormCs';
